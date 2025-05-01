f2a03fed9d20291906da39ca1d4538dff32614c8
Tushar Baranwal <tushar.baranwal@hcltech.com>
2025-05-01 11:41:10 +0530

    in chater6 problem10 i used list to print reverse

--- Chapter7-ps/problem10.py
+++ Chapter7-ps/problem10.py
@@ -1,5 +1,14 @@
-n = int(input("Enter the number : "))
+# n = int(input("Enter the number : "))
+# for i in range(1,11):
+#    print(f"{n}*{11-i} = {n*(11-i)}")
+
+
+
+#second method using list to print reverse table
+list =[]
+n = int(input("Enter a number"))
 for i in range(1,11):
-   print(f"{n}*{11-i} = {n*(11-i)}")
+    list.append(f"{n}*{i} = {n*i}")
 
-
+for reverse in reversed(list):
+    print(reverse)
