d9210d5212ee408290c57ddec24148e107bcef34
Tushar Baranwal <tushar.baranwal@hcltech.com>
2025-05-05 16:19:38 +0530

    Chapter9 done

---
+++ Chapter9/01_file.py
@@ -0,0 +1,5 @@
+# RAM = temporarily data store
+f = open("file.txt")
+data = f.read()
+print(data)
+f.close()
---
+++ Chapter9/02_file_write.py
@@ -0,0 +1,8 @@
+st = "Hey Tushar you are amazing..."
+
+f = open("myfile.txt","w")
+# f = open("myfile.txt","a") #append -a
+
+f.write(st)
+
+f.close()
